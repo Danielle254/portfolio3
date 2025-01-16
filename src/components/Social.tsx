c6d928f952f3e93d5cdb3966aa50b3abdf1612f8
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-01-16 14:12:49 -0700

    refactor: separate out the social icons into their own component

---
+++ src/components/Social.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import emailIcon from '../assets/email.png'
+import githubIcon from '../assets/github.png'
+import linkedinIcon from '../assets/linkedin.png'
+
+export default function Social() {
+  return (
+
+    <ul className='h-8 flex flex-row mx-auto gap-6'>
+        <li><a href="https://www.linkedin.com/in/danielle-lindblom/"><img src={linkedinIcon.src} className='w-8' /></a></li>
+        <li><a href="https://github.com/Danielle254"><img src={githubIcon.src} className='w-8' /></a></li>
+        <li><a href="mailto:danielle.lindblom@gmail.com"><img src={emailIcon.src} className='w-8' /></a></li>
+    </ul>
+
+  )
+}
