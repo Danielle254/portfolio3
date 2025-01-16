7c5b6acdf284384943f0024c6909a8abb7d90a86
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-01-16 15:26:39 -0700

    refactor: move text-center to a level higher

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,8 @@ import Social from './Social'
 
 export default function Sidebar() {
   return (
-    <div className='bg-gray-50 col-span-1 h-screen flex flex-col justify-between py-12'>
+    <div className='bg-gray-50 col-span-1 h-screen flex flex-col justify-between py-12  text-center'>
+      <h1>Danielle Lindblom</h1>
       <Nav />
       <Social />
     </div>
