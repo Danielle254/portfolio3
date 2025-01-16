2bd9f9ef90a4fbe3ce22327e0423dc3be1a8cad3
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-01-16 14:13:59 -0700

    style: adjust background gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-br from-violet-950 to-orange-900 min-h-screen">
+    <div className="bg-gradient-to-br from-violet-950 to-pink-950 to-95% min-h-screen">
       <h1 className="text-gray-50 text-2xl text-center pt-16">STUFF AND THINGS</h1>
     </div>
   );
