3ed8e393a8d8f40d057e6a6cfacb724ed1815c4f
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-01-16 15:25:41 -0700

    feat: add content to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Button from "@/components/Button";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-gray-50 text-2xl text-center pt-16">STUFF AND THINGS</h1>
+    <div className="max-w-5xl mx-auto min-h-screen text-center text-gray-50">
+      <h2 className="text-2xl ">Danielle Lindblom</h2>
+      <h3>Frontend Developer</h3>
+      <Button 
+      style='primary' 
+      text='Button Text' 
+      />
     </div>
   );
 }
