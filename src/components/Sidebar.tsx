a81c447be199c5fee56330bb71d27ed80871c1b5
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-11 13:20:00 -0700

    feat: add profile picture to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Nav from './Nav'
 import Social from './Social'
+import profile from '../assets/profile.jpeg'
+import Image from 'next/image'
 
 export default function Sidebar() {
   return (
-    <div className='bg-gray-50 col-span-1 h-screen flex flex-col justify-between py-12  text-center'>
+    <div className='bg-gray-50 col-span-1 h-screen flex flex-col justify-between py-12 items-center'>
       <h1>Danielle Lindblom</h1>
+      <Image
+      src={profile}
+      alt='Danielle Lindblom profile picture'
+      style={{borderRadius: '50%', border: '1px solid black'}}
+      height={100}
+      />
       <Nav />
       <Social />
     </div>
