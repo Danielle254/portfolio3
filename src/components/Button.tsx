7834b9da8373ba6a7ccf7ab089f87e3f33c2d5b5
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-13 11:45:56 -0700

    style: adjust button styling and add option for link vs button for accessibility

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,17 +2,28 @@ import React from 'react'
 
 interface ButtonProps {
     text: string;
-    style: string
+    style: string;
+    type: string;
 }
 
-export default function Button({text, style}: ButtonProps) {
-    const base = "px-2 py-1 border border-2 rounded-sm text-sm rounded-sm text-sm"
-    const primary = base + " border-accent-yellow text-accent-yellow";
+export default function Button({text, style, type}: ButtonProps) {
+    const base = "px-2 py-1 rounded-sm text-lg font-bold shadow border-2"
+    const primary = base + " border-accent-orange text-accent-orange";
     const secondary = base + " border-main-pink text-main-pink";
 
-  return (
-    <button className={style === 'primary' ? primary : secondary}>
-      {text}
-    </button>
-  )
+  switch (type) {
+    case 'button':
+      return (
+        <button className={style === 'primary' ? primary : secondary}>
+          {text}
+        </button>
+      )
+    case 'link':
+      return (
+        <a className={style === 'primary' ? primary : secondary}>
+          {text}
+        </a>
+      )
+  }
+
 }
