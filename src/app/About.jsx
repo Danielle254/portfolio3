f5c0e0e3c99f3e07042171bad9b93438c9840c6e
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-13 07:38:12 -0700

    feat: add section id for linking in navbar

--- src/app/About.jsx
+++ src/app/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='h-full max-w-5xl mx-auto'>
+    <div className='h-full max-w-5xl mx-auto' id='about-section'>
       <h2 className='text-xl'>Hi, I'm Danielle</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laborum nostrum, atque temporibus mollitia, similique quibusdam nobis blanditiis maiores nihil velit! Quo provident cumque, animi repellat corporis possimus quis expedita.</p>
     </div>
--- src/app/Projects.jsx
+++ src/app/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <div className='h-full max-w-5xl mx-auto'>
+    <div className='h-full max-w-5xl mx-auto' id='projects-section'>
       <h2 className='text-xl'>Projects</h2>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum facere odio voluptas minus rem non quos ad laboriosam nisi? Nulla tempora illo placeat quidem veniam rerum eveniet, nihil deleniti praesentium.</p>
     </div>
