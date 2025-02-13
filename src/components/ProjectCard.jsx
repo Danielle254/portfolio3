1e115802b945228f69f43cfc21ea7d01e5c522e4
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-13 11:51:37 -0700

    style: responsive on mobile

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,7 +5,7 @@ import Button from './Button'
 
 export default function ProjectCard() {
   return (
-    <div className='bg-light-purple bg-opacity-10 flex flex-col gap-4 rounded shadow text-light-purple items-center p-4'>
+    <div className='bg-light-purple bg-opacity-10 flex flex-col gap-4 rounded shadow text-light-purple items-center p-4 mx-1'>
       <h3 className='text-2xl font-bold'>Service Dogs Around Town</h3>
       <p className='text-lg'>A personal project that helps service dog handlers (persons with disabilities) to navigate their local communities.</p>      
       <Image 
@@ -13,14 +13,14 @@ export default function ProjectCard() {
       height={400}
       alt='screenshot of service dogs around town project'
       />      
-      <ul className='flex flex-row wrap text-lg list-[circle] gap-8'>
+      <ul className='flex flex-row flex-wrap text-lg list-[circle] gap-x-8 justify-center'>
         <li>React.js</li>
         <li>JavaScript</li>
         <li>Tailwind CSS</li>
         <li>Google Maps API</li>
         <li>Firebase</li>
       </ul>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-4 flex-wrap justify-center">
         <Button
         text={'Demo & Details'}
         style={'primary'}
