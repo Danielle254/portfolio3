1ddaca7675e0433ca176e76ea1d01b5d061811af
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-13 07:13:44 -0700

    style: update font color to match theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Projects from './Projects'
 
 export default function Home() {
   return (
-    <div className="h-full overflow-y-scroll text-center text-gray-50">
+    <div className="h-full overflow-y-scroll text-center text-light-purple">
       <About />
       <Projects />
     </div>
