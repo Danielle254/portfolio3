1a6e4c0b16ebd2e855854291780bf87615468a93
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-13 14:48:01 -0700

    style: adjust styling on project cards and ensure mobile responsiveness

--- src/app/Projects.jsx
+++ src/app/Projects.jsx
@@ -5,26 +5,20 @@ const projectData = [
   {
       title: "Service Dogs Around Town",
       summary: "A personal project that helps service dog handlers (persons with disabilities) navigate their local communities, tracking information that relates to the comfort and safety of their service animals.",
+      img: "/projectImages/SDATcover.png",
       alt: "screenshot of service dogs around town project",
-      tags: ["React.js", "JavaScript", "Tailwind CSS", "Google Maps API", "Firebase"],
+      tags: ["React.js", "JavaScript", "Tailwind CSS", "Google Maps API", "Firebase", "Accessibility"],
       link1: "/sdat",
       link2: "https://servicedogsaroundtown.vercel.app/"
   },
   {
-      title: "Service Dogs Around Town",
-      summary: "A personal project that helps service dog handlers (persons with disabilities) navigate their local communities, tracking information that relates to the comfort and safety of their service animals.",
+      title: "Gridiron Survivor",
+      summary: "Football survivor pool web application built as part of an apprenticeship program. Team project involving agile sprint planning, unit testing, code reviews, and a real customer.",
+      img: "/projectImages/GIScover.png",
       alt: "screenshot of service dogs around town project",
-      tags: ["React.js", "JavaScript", "Tailwind CSS", "Google Maps API", "Firebase"],
-      link1: "/sdat",
-      link2: "https://servicedogsaroundtown.vercel.app/"
-  },
-  {
-      title: "Service Dogs Around Town",
-      summary: "A personal project that helps service dog handlers (persons with disabilities) navigate their local communities, tracking information that relates to the comfort and safety of their service animals.",
-      alt: "screenshot of service dogs around town project",
-      tags: ["React.js", "JavaScript", "Tailwind CSS", "Google Maps API", "Firebase"],
-      link1: "/sdat",
-      link2: "https://servicedogsaroundtown.vercel.app/"
+      tags: ["Next.js", "TypeScript", "React.js", "shadcn/ui", "Jest/React Testing Library"],
+      link1: "/gis",
+      link2: "https://www.staging.gridironsurvivor.com/"
   }
 ]
 
@@ -41,7 +35,7 @@ export default function Projects() {
   return (
     <div className='h-full md:min-h-screen max-w-4xl mx-auto py-8 px-2' id='projects-section'>
       <h2 className='text-4xl mb-10'>Projects</h2>
-      <div className='flex flex-col gap-10'>
+      <div className='flex flex-col gap-10 items-center'>
         {projectsList}
       </div>
       
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,37 +1,43 @@
 import React from 'react'
 import Image from 'next/image'
-import SDATcover from '../assets/SDAT cover.png'
 import Button from './Button'
 
-export default function ProjectCard() {
+
+export default function ProjectCard({project, index}) {
+
   return (
-    <div className='bg-light-purple bg-opacity-10 flex flex-col gap-4 rounded shadow text-light-purple items-center p-4 mx-1'>
-      <h3 className='text-2xl font-bold'>Service Dogs Around Town</h3>
-      <p className='text-lg'>A personal project that helps service dog handlers (persons with disabilities) to navigate their local communities.</p>      
+    <div className='bg-light-purple bg-opacity-10 flex flex-col items-center text-light-purple rounded shadow max-w-4xl p-4 mx-2 md:px-36 md:py-8 my-10' key={index}>
+      <h3 className='text-3xl font-bold mb-8'>{project.title}</h3>
+      <p className='text-lg text-left'>{project.summary}</p>      
       <Image 
-      src={SDATcover}
-      height={400}
-      alt='screenshot of service dogs around town project'
+        src={project.img}
+        alt={project.alt}
+        sizes='100vw'
+        width={0}
+        height={0}
+        style={{borderRadius: '5px', border: '1px solid #37183b', width: '100%', margin: '16px 0', height: 'auto'}}
       />      
-      <ul className='flex flex-row flex-wrap text-lg list-[circle] gap-x-8 justify-center'>
-        <li>React.js</li>
-        <li>JavaScript</li>
-        <li>Tailwind CSS</li>
-        <li>Google Maps API</li>
-        <li>Firebase</li>
+      <ul className='flex flex-row font-bold flex-wrap text-lg list-[circle] gap-x-8 justify-center mb-8'>
+        {
+          project.tags.map((tag) => {return <li>{tag}</li>})
+        }
       </ul>
       <div className="flex flex-row gap-4 flex-wrap justify-center">
         <Button
-        text={'Demo & Details'}
-        style={'primary'}
-        type={'link'}
+          text={'Demo & Details'}
+          style={'primary'}
+          type={'link'}
+          destination={project.link1}
+          newWindow={false}
         />
         <Button
-        text={'View Website'}
-        style={'primary'}
-        type={'link'}
+          text={'Visit Website'}
+          style={'primary'}
+          type={'link'}
+          destination={project.link2}
+          newWindow={true}
         />
-      </div>      
-    </div>
-  )
+      </div>          
+  </div>
+    )
 }
