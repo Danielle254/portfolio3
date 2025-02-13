2660c3e7c7a5258bee23ff1f546ee868393cbd80
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-13 07:38:33 -0700

    style: update social icon hover color to match new accent color

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -7,9 +7,9 @@ export default function Social() {
   return (
 
     <ul className='h-8 flex flex-row mx-auto gap-6'>
-        <li><a href="https://www.linkedin.com/in/danielle-lindblom/"><img src={linkedinIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#a73974] rounded-full' /></a></li>
-        <li><a href="https://github.com/Danielle254"><img src={githubIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#a73974] rounded-full' /></a></li>
-        <li><a href="mailto:danielle.lindblom@gmail.com"><img src={emailIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#a73974] rounded-full' /></a></li>
+        <li><a href="https://www.linkedin.com/in/danielle-lindblom/"><img src={linkedinIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#ffbd59] rounded-full' /></a></li>
+        <li><a href="https://github.com/Danielle254"><img src={githubIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#ffbd59] rounded-full' /></a></li>
+        <li><a href="mailto:danielle.lindblom@gmail.com"><img src={emailIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#ffbd59] rounded-full' /></a></li>
     </ul>
 
   )
