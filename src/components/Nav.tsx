f1a27324446671695a4cd0555e553cee61b872b4
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-13 07:46:58 -0700

    fix: link to resume that opens file in new tab

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,10 +12,10 @@ export default function Nav() {
               <Link href='#projects-section'>Projects</Link>
             </li>
             <li>
-              <a href='../../public/resume.pdf' target='_blank' className='flex flex-row items-center gap-1'>Resume
+              <Link href='/resume.pdf' target='_blank' locale={false} className='flex flex-row items-center gap-1'>Resume
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4"  aria-hidden='true'>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-              </svg></a>
+              </svg></Link>
             </li>
           </ul>
         </nav>
