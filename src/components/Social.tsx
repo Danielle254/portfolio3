e995d996769af1adf1549f0ed68b0f8d1101ad81
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-14 14:34:37 -0700

    a11y: add titles to social link icons and blank alt text for the images

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -7,9 +7,9 @@ export default function Social() {
   return (
 
     <ul className='h-8 flex flex-row mx-auto gap-2 lg:gap-6'>
-        <li><a href="https://www.linkedin.com/in/danielle-lindblom/"><img src={linkedinIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#ffbd59] rounded-full' /></a></li>
-        <li><a href="https://github.com/Danielle254"><img src={githubIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#ffbd59] rounded-full' /></a></li>
-        <li><a href="mailto:danielle.lindblom@gmail.com"><img src={emailIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#ffbd59] rounded-full' /></a></li>
+        <li><a href="https://www.linkedin.com/in/danielle-lindblom/" title='LinkedIn'><img src={linkedinIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#ffbd59] rounded-full' alt=''/></a></li>
+        <li><a href="https://github.com/Danielle254" title='GitHub'><img src={githubIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#ffbd59] rounded-full' alt=''/></a></li>
+        <li><a href="mailto:danielle.lindblom@gmail.com" title='Email'><img src={emailIcon.src} className='w-8 hover:shadow-[0_0_10px_5px_#ffbd59] rounded-full' alt=''/></a></li>
     </ul>
 
   )
