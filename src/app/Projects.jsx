51030f8879aae2c437759c63a2b15afadfc82afb
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-14 13:17:29 -0700

    style: adjust sizing for container

--- src/app/Projects.jsx
+++ src/app/Projects.jsx
@@ -33,12 +33,11 @@ const projectsList = projectData.map((project, index) => {
 
 export default function Projects() {
   return (
-    <div className='h-full md:min-h-screen max-w-4xl mx-auto py-8 px-2' id='projects-section'>
+    <div className='h-full max-w-5xl mx-auto py-8 px-2' id='projects-section'>
       <h2 className='text-4xl mb-10'>Projects</h2>
       <div className='flex flex-col gap-10 items-center'>
         {projectsList}
-      </div>
-      
+      </div>      
     </div>
   )
 }
