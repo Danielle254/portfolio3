3ebeade6737e744f2e9e1ce4dad77848cf8c6348
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-14 13:17:41 -0700

    style: adjust font sizes for mobile

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,8 +7,8 @@ export default function ProjectCard({project, index}) {
 
   return (
     <div className='bg-light-purple bg-opacity-10 flex flex-col items-center text-light-purple rounded shadow max-w-4xl p-4 mx-2 md:px-36 md:py-8 my-6 md:my-10' key={index}>
-      <h3 className='text-3xl font-bold mb-8'>{project.title}</h3>
-      <p className='text-lg text-left'>{project.summary}</p>      
+      <h3 className='text-2xl md:text-3xl font-bold mb-8'>{project.title}</h3>
+      <p className='text-md md:text-lg text-left'>{project.summary}</p>      
       <Image 
         src={project.img}
         alt={project.alt}
@@ -17,7 +17,7 @@ export default function ProjectCard({project, index}) {
         height={0}
         style={{borderRadius: '5px', border: '1px solid #37183b', width: '100%', margin: '16px 0', height: 'auto'}}
       />      
-      <ul className='flex flex-row font-bold flex-wrap text-lg list-[circle] gap-x-8 justify-center mb-8'>
+      <ul className='flex flex-row font-bold flex-wrap text-md md:text-lg list-[circle] gap-x-8 justify-center mb-8'>
         {
           project.tags.map((tag) => {return <li>{tag}</li>})
         }
