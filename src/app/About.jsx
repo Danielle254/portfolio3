d0e2546b7298421fb729e53db71bd4b7daf4226c
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-14 13:16:32 -0700

    feat: add copy to about section

--- src/app/About.jsx
+++ src/app/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Caveat } from 'next/font/google'
+
+const caveat = Caveat({ subsets: ['latin'], weight: '400', variable: '--font-caveat' });
 
 export default function About() {
   return (
     <div className='h-full md:min-h-screen max-w-4xl mx-auto py-8 px-2' id='about-section'>
-      <h2 className='text-2xl'>Hi, I'm Danielle</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laborum nostrum, atque temporibus mollitia, similique quibusdam nobis blanditiis maiores nihil velit! Quo provident cumque, animi repellat corporis possimus quis expedita.</p>
+      <h2 className={`text-5xl ${caveat.variable} font-special my-4 md:mb-2`}>Hi, I'm Danielle</h2>
+      <h3 className='text-xl mb-8'>a <span className='font-bold text-accent-orange'>Frontend Developer</span> passionate about accessibility</h3>
+      <div className='relative max-w-2xl mx-auto h-0 pb-[56%] lg:pb-[40%] border border-main-pink rounded'>
+          <iframe className='absolute w-full h-full left-0 top-0' src="https://www.loom.com/embed/7780d925a77e48848bc75caff6d317c3?sid=fc473fda-2fc0-4b7e-b0ce-ba8106adae53?hideEmbedTopBar=true&hide_title=true&hide_owner=true"  webkitallowfullscreen={'true'} mozallowfullscreen={'true'} allowFullScreen title='Danielle Lindblom video introduction' ></iframe>
+      </div>
+      <p className='text-md md:text-lg mt-8'>I love working with <span className='text-accent-orange font-bold'>React, TypeScript, and Next.js.</span></p>
+      <p className='text-md md:text-lg'>I have a diverse background in behavior science, project management, and instruction.</p>
     </div>
   )
 }
