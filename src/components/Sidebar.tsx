5f81e3cc73ee0a6039e91f77fd14376df107d2f4
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-14 12:16:14 -0700

    style: update sidebar background color to match theme

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,7 +17,7 @@ export default function Sidebar() {
 
   return (
     <>
-      <div className='hidden bg-gray-100 h-screen md:flex flex-col justify-between py-12 items-center col-span-1 text-main-purple'>
+      <div className='hidden bg-light-purple h-screen md:flex flex-col justify-between py-12 items-center col-span-1 text-main-purple'>
         <div className='flex flex-col gap-4 items-center'>
           <h1 className='text-lg font-bold text-center'>Danielle Lindblom</h1>
           <Image
