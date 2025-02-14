a6b09635b6de4fcdf1851123d138beb5e434c3a0
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-14 14:29:38 -0700

    copy: adjust wording in hero section

--- src/app/About.jsx
+++ src/app/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
           <iframe className='absolute w-full h-full left-0 top-0' src="https://www.loom.com/embed/7780d925a77e48848bc75caff6d317c3?sid=fc473fda-2fc0-4b7e-b0ce-ba8106adae53?hideEmbedTopBar=true&hide_title=true&hide_owner=true"  webkitallowfullscreen={'true'} mozallowfullscreen={'true'} allowFullScreen title='Danielle Lindblom video introduction' ></iframe>
       </div>
       <p className='text-md md:text-lg mt-8'>I love working with <span className='text-accent-orange font-bold'>React,</span> <span className='text-accent-orange font-bold'>TypeScript,</span> and <span className='text-accent-orange font-bold'>Next.js.</span></p>
-      <p className='text-md md:text-lg'>I have a diverse background in behavior science, project management, and instruction.</p>
+      <p className='text-md md:text-lg'>My background includes behavior science, project management, and instruction.</p>
       <div className='flex flex-row gap-8 justify-center mt-8'>
         <Button
           text='View Projects'
