d71ebd5fe59bc60ff0a1f289e00d28f1db0f4a95
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-18 09:45:12 -0700

    feat: create footer

---
+++ src/app/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='text-md text-light-purple'>
+      <p>Built with Next.js, TypeScript, and Tailwind CSS. <a href="https://github.com/Danielle254/portfolio3" className='underline'>View Code</a></p>      
+    </div>
+  )
+}
