cc44c6624fde7a8d77ebf7ab78cd560cea2aec27
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-18 12:47:19 -0700

    feat: add feature photo to connect section and adjust section styling

--- src/app/Connect.jsx
+++ src/app/Connect.jsx
@@ -2,29 +2,45 @@ import React from 'react'
 import emailIcon from '../../public/socialIcons/emailLight.png'
 import githubIcon from '../../public/socialIcons/githubLight.png'
 import linkedinIcon from '../../public/socialIcons/linkedinLight.png'
+import montana from '../../public/montana.jpg'
+import Image from 'next/image'
+import Button from '@/components/Button'
 import Footer from './Footer.jsx'
 
 export default function Connect() {
   return (
-    <div className='max-w-5xl flex flex-col items-center justify-between mx-auto pt-8 pb-6 md:pb-10 px-2 md:px-0 min-h-screen' id='connect-section'>
-      <div className='flex flex-col items-center'>
-        <h2 className='text-4xl mb-24 text-accent-orange'>Connect</h2>
-        <ul className='text-light-purple flex flex-col gap-8'>
-          <li className='text-lg hover:shadow-[0_0_10px_5px_#ffbd59] hover:rounded px-2 py-1'><a href="https://www.linkedin.com/in/danielle-lindblom/" className='flex flex-row gap-4 items-center'><img src={linkedinIcon.src} className='w-6 h-6' alt=''/>LinkedIn 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4"  aria-hidden='true'>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-            </svg></a>
-          </li>
-          <li className='text-lg hover:shadow-[0_0_10px_5px_#ffbd59] hover:rounded px-2 py-1'><a href="https://github.com/Danielle254" className='flex flex-row gap-4 items-center'><img src={githubIcon.src} className='w-6 h-6' alt=''/>GitHub 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4"  aria-hidden='true'>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-            </svg></a>
-          </li>
-          <li className='text-lg hover:shadow-[0_0_10px_5px_#ffbd59] hover:rounded px-2 py-1'><a href="mailto:danielle.lindblom@gmail.com" className='flex flex-row gap-4 items-center
-          '><img src={emailIcon.src} className='w-6 h-6' alt=''/>Email</a>
-          </li>
-        </ul>
-        <p className='text-lg mt-16'>Currently residing in Missoula, Montana and open to remote work.</p>
+    <div className='max-w-5xl mx-auto flex flex-col items-center pt-8 pb-6 md:pb-8 px-2 md:px-0' id='connect-section'>
+      <h2 className='text-4xl mb-10 md:mb-16 text-accent-orange'>Connect</h2>
+      <ul className='text-light-purple flex flex-col gap-1 md:flex-row md:gap-10'>
+        <li className='text-lg hover:shadow-[0_0_10px_5px_#ffbd59] hover:rounded px-2 py-1'><a href="https://www.linkedin.com/in/danielle-lindblom/" className='flex flex-row gap-4 md:gap-2 items-center'><img src={linkedinIcon.src} className='w-6 h-6' alt=''/>LinkedIn 
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4"  aria-hidden='true'>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+          </svg></a>
+        </li>
+        <li className='text-lg hover:shadow-[0_0_10px_5px_#ffbd59] hover:rounded px-2 py-1'><a href="https://github.com/Danielle254" className='flex flex-row gap-4 md:gap-2 items-center'><img src={githubIcon.src} className='w-6 h-6' alt=''/>GitHub 
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4"  aria-hidden='true'>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+          </svg></a>
+        </li>
+        <li className='text-lg hover:shadow-[0_0_10px_5px_#ffbd59] hover:rounded px-2 py-1'><a href="mailto:danielle.lindblom@gmail.com" className='flex flex-row gap-4 md:gap-2 items-center
+        '><img src={emailIcon.src} className='w-6 h-6' alt=''/>Email</a>
+        </li>
+      </ul>
+      <div className='flex flex-col items-center mt-8 mb-6 bg-light-purple bg-opacity-10 rounded shadow px-10 py-6 mx-2 md:mx-0'>
+        <Image
+          src={montana}
+          alt='Danielle Lindblom in Glacier National Park'
+          style={{borderRadius: '10%', border: '1px solid #37183b'}}
+          height={300}
+        />          
+        <p className='text-lg mt-2 mb-6'>Currently residing in Missoula, Montana and open to remote work.</p>
+        <Button
+          text={'View Resume'}
+          style={'primary'}
+          type={'link'}
+          destination={'/resume.pdf'}
+          newWindow={true}
+        />
       </div>
       <Footer />
     </div>
