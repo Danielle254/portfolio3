a044641d0b80edbaf6a0403722e6353420b1d259
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-18 09:45:42 -0700

    feat: add connect section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from './About'
 import Projects from './Projects'
+import Connect from './Connect'
 
 export default function Home() {
   return (
     <div className="h-full md:overflow-y-scroll text-center text-light-purple scroll-smooth">
       <About />
       <Projects />
+      <Connect />
     </div>
   );
 }
