d706fcac5cfda16012cce6ce2ab29b6ce7bd3a5f
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-21 10:43:46 -0700

    style: adjust border radius on iframe

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -18,8 +18,8 @@ export default function About() {
     >
       <h2 className={`text-5xl ${caveat.variable} font-special my-4 md:mb-2`}>Hi, I'm Danielle</h2>
       <h3 className='text-xl mb-8'>a <span className='font-bold text-accent-orange'>Frontend Developer</span> passionate about accessibility.</h3>
-      <div className='relative max-w-2xl mx-auto h-0 pb-[56%] lg:pb-[40%] border border-main-pink rounded'>
-        <iframe className='absolute w-full h-full left-0 top-0' src="https://www.loom.com/embed/7780d925a77e48848bc75caff6d317c3?sid=fc473fda-2fc0-4b7e-b0ce-ba8106adae53?hideEmbedTopBar=true&hide_title=true&hide_owner=true" allowFullScreen title='Danielle Lindblom video introduction' ></iframe>
+      <div className='relative max-w-2xl mx-auto h-0 pb-[56%] lg:pb-[40%]'>
+        <iframe className='absolute w-full h-full left-0 top-0 border border-main-pink rounded' src="https://www.loom.com/embed/7780d925a77e48848bc75caff6d317c3?sid=fc473fda-2fc0-4b7e-b0ce-ba8106adae53?hideEmbedTopBar=true&hide_title=true&hide_owner=true" allowFullScreen title='Danielle Lindblom video introduction' ></iframe>
       </div>
       <p className='text-md md:text-lg mt-8'>I love working with <span className='text-accent-orange font-bold'>React,</span> <span className='text-accent-orange font-bold'>TypeScript,</span> and <span className='text-accent-orange font-bold'>Next.js.</span></p>
       <p className='text-md md:text-lg'>My background includes behavior science, project management, and instruction.</p>
