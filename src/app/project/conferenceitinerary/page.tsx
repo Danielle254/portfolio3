ca6db40d86dc1b1c6d852b027da33435298de2d5
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-24 15:45:41 -0700

    feat: add content for project detail page

--- src/app/project/conferenceitinerary/page.tsx
+++ src/app/project/conferenceitinerary/page.tsx
@@ -3,39 +3,42 @@
 import React from 'react'
 import ProjectDetailPage from '../../../components/ProjectDetailPage'
 
-const gisProject = {
-  title: "Gridiron Survivor",
+const conferenceProject = {
+  title: "Create Your Own Personal Itinerary from a Conference Agenda",
   demo: "https://www.loom.com/embed/e32f21774c3a4a4d8a9a402abb4b5a81?hideEmbedTopBar=true",
-  tech: ["Next.js", "styleScript", "React.js", "Jest", "React Testing Library", "shadcn/ui"],
-  description: `Gridiron Survivor is a football survivor pool web application built as part of a software development apprenticeship program. I worked on this project from October 2024 - February 2025, focusing on addressing user experience issues including accessibility. We are currently in the process of standing up a new team project.
+  tech: ["React", "JavaScript", "CSS", "Format PDF", "Local Storage"],
+  description: `This is a project of my own idea and design. The site displays the agenda for a conference in chronological order (data stored in separate file), then allows the user to add activities from the agenda to their own personal itinerary.
 
-  This site allows users to stand up a league with several members, then those members can make multiple picks from a list of NFL games happening that week. The game's outcome determines whose picks "survive" and live to do another round the next week. Whoever is still standing at the end of the season typically wins a large prize!
+  This is something I wish I had when attending conferences in the past! I'd be circling my preferred talks on a paper printout. This is especially helpful when there are multiple talks occurring at the same time. When adding talks to their itinerary, they sort automatically in chornological order.
+
+  Finally, there is the option to print your itinerary, which is formatted for ease of use.
   
-  October 2024 - Present`,
-  heading: "Key Contributions",
-  features: ["Removed distracting, obsolete information from the site", "Gave users the ability to clear popup notifications", "Resolved a screen flickering issue by creating a new login authentication workflow and utilizing a loading spinner", "Identified and fixed a key accessibility issue with the menu controls"],
-  skills: ["Getting up to speed quickly on an unfamiliar, complex code base", "Quickly learning new technologies (Next.js, styleScript, Jest, React Testing Library)", "Performing peer code reviews", "Writing unit tests for improved code quality" ],
-  learnings: `This project has been a great experience in implementing typical corporate processes like sprint planning, estimating points for tickets, and performing code reviews. I really enjoyed the challenge of diving into an existing codebase, finding the files relevant to my tickets, solving problems, and working collaboratively with a team.
+  July 2024`,
+  heading: "Key Features",
+  features: ["Talks display in chronological order automatically, regardless of order added to itinerary", "User's itinerary stays put, even if they navigate away or refresh the screen. This makes it easy to come back and make changes as needed without having the build their itinerary from scatch each time.", "Option to clear the itinerary and start over", "Ability to print a nicely formatted version of your personal itinerary"],
+  skills: ["React components", "Use of state, props, hooks, and event handlers", "Utilizing local storage", "Sort function that uses the Date object", "Passing information from parent to child and reverse", "Responsive website", "Flexbox and Grid" ],
+  learnings: `This project really required me to be organized with my file structure, which in turn kept me organized in the execution.
   
-  This was my first introduction to Next.js and styleScript, and I really like these tools and the structure they provide. It was a good resourcefulness exercise in learning these quickly so that I could be effective as soon as possible.
+  I liked learning about local storage and the print function. I can see these being useful in many other applications.
+
+  I appreciated that my understanding of how to pass functions and attributes to children components really came together through this project.
   
-  Finally, I was glad for an opportunity to flex my web accessibility skills and make proactive contributions to help make this project more accessible.`,
-  future: `The team is shifting to a different project going forward - a Secret Santa web application that will be launched as a paid SaaS product late 2025. This project utilizes a similar tech stack as well as an AI API for customized gift recommendations.`,
+  I stepped away from this project for a few months, and when I returned, I was able to read through and understand the code base and how it was functioning. I think this is largely due to having clean, readable code that is well-labeled.`,
+  future: `I'm actually planning another project like this where I create the conference agenda with some sort of library to display it visually in a more calendar-like format. This will make it easier to tell which talks happen at the same time, and even visually distinguish the locations of the talks.
+  
+  I'd also like to add a "share" button in addition to printing.
+
+  Another idea would be to add "add to calendar" options so users could put the talks directly onto their own personal calendar (google, etc).`,
   links: [
     {
       title: "View Website",
-      link: 'https://staging.gridironsurvivor.com/',
-      style: 'primary'
+      link: 'https://danielle254.github.io/Conference-Agenda/',
+      style: "primary"
     },
     {
       title: "View Code",
-      link: 'https://github.com/LetsGetTechnical/gridiron-survivor',
-      style: 'secondary'
-    },
-    {
-      title: "Pull Requests",
-      link: "https://github.com/LetsGetTechnical/gridiron-survivor/pulls?q=is%3Apr+author%3A%40me+is%3Aclosed",
-      style: 'secondary'
+      link: 'https://github.com/Danielle254/Conference-Agenda',
+      style: "secondary"
     }
   ]
 }
@@ -45,7 +48,7 @@ export default function ConferenceItinerary() {
   return (
     <>
     <ProjectDetailPage 
-    project={gisProject}
+    project={conferenceProject}
     />
     </>
   )
