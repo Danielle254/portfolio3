206e718450a57cee35311a5eb7e8c41a9ada6552
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-24 15:44:54 -0700

    style: change style of primary dark button and adjust TS typing for props

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 interface CustomLinkProps {
     text: string;
-    style: "primary" | "secondary" | "primaryDark" | "secondaryDark";
+    style: string;
     destination: string;
     newWindow: boolean;
 }
@@ -12,7 +12,7 @@ export default function CustomLink({text, style, destination, newWindow}: Custom
     const base = "px-2 py-1 rounded-sm text-lg shadow border-2 cursor-pointer"
     const primary = base + " border-accent-orange text-accent-orange hover:bg-accent-orange hover:text-main-purple hover:bg-opacity-60";
     const secondary = base + " border-light-purple text-light-purple hover:bg-light-purple hover:text-main-purple hover:bg-opacity-60";
-    const primaryDark = base + " border-main-purple bg-main-purple text-light-purple hover:bg-transparent hover:text-main-purple";
+    const primaryDark = base + " border-main-purple bg-main-purple text-light-purple hover:bg-main-purple hover:text-light-purple hover:bg-opacity-60";
     const secondaryDark = base + " border-main-purple text-main-purple hover:bg-main-purple hover:text-light-purple hover:bg-opacity-60"
 
   switch (style) {
