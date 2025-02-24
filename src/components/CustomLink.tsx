d1b74a4ee8a9e39c35fd45ade0cd4f9defc3a990
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-24 12:23:16 -0700

    feat: create new CustomLink component to replace old Button component

---
+++ src/components/CustomLink.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Link from 'next/link';
+
+interface CustomLinkProps {
+    text: string;
+    style: "primary" | "secondary" | "dark";
+    destination: string;
+    newWindow: boolean;
+}
+
+export default function CustomLink({text, style, destination, newWindow}: CustomLinkProps) {
+    const base = "px-2 py-1 rounded-sm text-lg shadow border-2 cursor-pointer"
+    const primary = base + " border-accent-orange text-accent-orange hover:bg-accent-orange hover:text-main-purple hover:bg-opacity-60";
+    const secondary = base + " border-light-purple text-light-purple hover:bg-light-purple hover:text-main-purple hover:bg-opacity-60";
+    const dark = base + " border-main-purple text-main-purple hover:bg-main-purple hover:text-light-purple hover:bg-opacity-60";
+
+
+  switch (style) {
+    case 'primary':
+      return (
+        <Link
+        href={destination}
+        className={primary}
+        target={newWindow ? '_blank' : '_self'}
+        >{text}</Link>
+      )
+    case 'secondary':
+        return (
+            <Link
+            href={destination}
+            className={secondary}
+            target={newWindow ? '_blank' : '_self'}
+            >{text}</Link>
+        )
+    case 'dark':
+        return (
+        <Link
+        href={destination}
+        className={dark}
+        target={newWindow ? '_blank' : '_self'}
+        >{text}</Link>
+        )
+  }
+
+}
