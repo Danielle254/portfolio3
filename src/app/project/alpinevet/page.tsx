9f3f5ca2dd489a763150bf442a0f46ade017b90a
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-24 14:15:50 -0700

    feat: add copy to alpine vet project detail page

--- src/app/project/alpinevet/page.tsx
+++ src/app/project/alpinevet/page.tsx
@@ -4,39 +4,40 @@ import React from 'react'
 import ProjectDetailPage from '../../../components/ProjectDetailPage'
 
 const gisProject = {
-  title: "Gridiron Survivor",
-  demo: "https://www.loom.com/embed/e32f21774c3a4a4d8a9a402abb4b5a81?hideEmbedTopBar=true",
-  tech: ["Next.js", "styleScript", "React.js", "Jest", "React Testing Library", "shadcn/ui"],
-  description: `Gridiron Survivor is a football survivor pool web application built as part of a software development apprenticeship program. I worked on this project from October 2024 - February 2025, focusing on addressing user experience issues including accessibility. We are currently in the process of standing up a new team project.
+  title: "Local Business Homepage Refresh",
+  demo: "https://www.loom.com/embed/41d9ec8e943c43bd945eeddbea745b5a?hideEmbedTopBar=true",
+  tech: ["HTML", "CSS", "Accessibility"],
+  description: `I took the website of a local business that I frequent, Alpine Vet Care in Missoula, and gave their website homepage a refresh. This project showcases my ability to use CSS with no libraries or frameworks to create a responsive webpage for desktop and mobile.
 
-  This site allows users to stand up a league with several members, then those members can make multiple picks from a list of NFL games happening that week. The game's outcome determines whose picks "survive" and live to do another round the next week. Whoever is still standing at the end of the season typically wins a large prize!
+  Their existing website was crowded, outdated, and had several accessibility issues.
+
+  I wanted to create a more user-friendly, easy to navigate homepage for the veterinarian's customers.
   
-  October 2024 - Present`,
+  July 2024`,
   heading: "Key Contributions",
   features: ["Removed distracting, obsolete information from the site", "Gave users the ability to clear popup notifications", "Resolved a screen flickering issue by creating a new login authentication workflow and utilizing a loading spinner", "Identified and fixed a key accessibility issue with the menu controls"],
-  skills: ["Getting up to speed quickly on an unfamiliar, complex code base", "Quickly learning new technologies (Next.js, styleScript, Jest, React Testing Library)", "Performing peer code reviews", "Writing unit tests for improved code quality" ],
-  learnings: `This project has been a great experience in implementing typical corporate processes like sprint planning, estimating points for tickets, and performing code reviews. I really enjoyed the challenge of diving into an existing codebase, finding the files relevant to my tickets, solving problems, and working collaboratively with a team.
+  skills: ["Semantic HTML", "Pure CSS - FlexBox, Grid, Media Queries", "Web Design and User Experience", "Web Accessibility (NOTE: the checkbox hack, described below, is NOT fully web accessible)", "Responsive Design" ],
+  learnings: `I used the "checkbox hack" for the mobile menu. I could have used JavaScript, but this was a fun feature to learn. The checkbox hack utilizes the "checked" vs "not checked" property of a checkbox instead of "onClick" in JavaScript. I also created the down/up arrows for the mobile submenu manually.
   
-  This was my first introduction to Next.js and styleScript, and I really like these tools and the structure they provide. It was a good resourcefulness exercise in learning these quickly so that I could be effective as soon as possible.
+  I created a 2-layer menu. It was interesting to learn different methods of creating a submenu for both dektop and mobile views. This is a very common feature in older or complex business websites.`,
+  future: `Creating a Site Menu in the footer. The footer is already quite full. I might make it it's own section and add a site menu to the actual footer on the desktop version.
   
-  Finally, I was glad for an opportunity to flex my web accessibility skills and make proactive contributions to help make this project more accessible.`,
-  future: `The team is shifting to a different project going forward - a Secret Santa web application that will be launched as a paid SaaS product late 2025. This project utilizes a similar tech stack as well as an AI API for customized gift recommendations.`,
+  I'd also like to add a "share" button in addition to printing.
+
+  It would be fun to do an entire website overhaul and build the other pages.
+
+  Add JavaScript for more simplified functionality of the navbar, and add a "return to top" icon for mobile.`,
   links: [
     {
       title: "View Website",
-      link: 'https://staging.gridironsurvivor.com/',
+      link: 'https://danielle254.github.io/Alpine-Vet/',
       style: "primary"
     },
     {
       title: "View Code",
-      link: 'https://github.com/LetsGetTechnical/gridiron-survivor',
+      link: 'https://github.com/Danielle254/Alpine-Vet',
       style: "secondary"
     },
-    {
-      title: "Pull Requests",
-      link: "https://github.com/LetsGetTechnical/gridiron-survivor/pulls?q=is%3Apr+author%3A%40me+is%3Aclosed",
-      style: "secondary"
-    }
   ]
 }
 
