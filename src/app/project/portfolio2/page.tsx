68b204e0acbd48b1113c615272ddeddb3435c429
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-25 08:48:15 -0700

    feat: add copy to project detail page

--- src/app/project/portfolio2/page.tsx
+++ src/app/project/portfolio2/page.tsx
@@ -3,17 +3,17 @@
 import React from 'react'
 import ProjectDetailPage from '../../../components/ProjectDetailPage'
 
-const gisProject = {
-  title: "Gridiron Survivor",
+const portfolio2Project = {
+  title: "Personal Portfolio - Version 2",
   demo: "https://www.loom.com/embed/e32f21774c3a4a4d8a9a402abb4b5a81?hideEmbedTopBar=true",
-  tech: ["Next.js", "styleScript", "React.js", "Jest", "React Testing Library", "shadcn/ui"],
+  tech: ["React.js", "JavaScript", "Sass/SCSS", "React Router", "Accessibility"],
   description: `Gridiron Survivor is a football survivor pool web application built as part of a software development apprenticeship program. I worked on this project from October 2024 - February 2025, focusing on addressing user experience issues including accessibility. We are currently in the process of standing up a new team project.
 
   This site allows users to stand up a league with several members, then those members can make multiple picks from a list of NFL games happening that week. The game's outcome determines whose picks "survive" and live to do another round the next week. Whoever is still standing at the end of the season typically wins a large prize!
   
-  October 2024 - Present`,
-  heading: "Key Contributions",
-  features: ["Removed distracting, obsolete information from the site", "Gave users the ability to clear popup notifications", "Resolved a screen flickering issue by creating a new login authentication workflow and utilizing a loading spinner", "Identified and fixed a key accessibility issue with the menu controls"],
+  July - September 2024`,
+  heading: "Key Features",
+  features: ["Light and Dark mode with toggle", "Ability for users to turn animations on/off", "Multi page React application"],
   skills: ["Getting up to speed quickly on an unfamiliar, complex code base", "Quickly learning new technologies (Next.js, styleScript, Jest, React Testing Library)", "Performing peer code reviews", "Writing unit tests for improved code quality" ],
   learnings: `This project has been a great experience in implementing typical corporate processes like sprint planning, estimating points for tickets, and performing code reviews. I really enjoyed the challenge of diving into an existing codebase, finding the files relevant to my tickets, solving problems, and working collaboratively with a team.
   
@@ -29,15 +29,12 @@ const gisProject = {
     },
     {
       title: "View Code",
-      link: 'https://github.com/LetsGetTechnical/gridiron-survivor',
+      link: 'https://github.com/Danielle254/portfolio-site2',
       style: 'secondary'
     },
-    {
-      title: "Pull Requests",
-      link: "https://github.com/LetsGetTechnical/gridiron-survivor/pulls?q=is%3Apr+author%3A%40me+is%3Aclosed",
-      style: 'secondary'
-    }
-  ]
+  ],
+  screen1: '/projectImages/portfolio2screen1.jpg',
+  screen2: '/projectImages/portfolio2screen2.jpg'
 }
 
 
@@ -45,7 +42,7 @@ export default function Portfolio2() {
   return (
     <>
     <ProjectDetailPage 
-    project={gisProject}
+    project={portfolio2Project}
     />
     </>
   )
