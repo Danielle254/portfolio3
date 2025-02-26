45adb602765a66df661c0889d47d5e7b590097aa
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-26 12:07:20 -0700

    fix: clicking social icons opened in same window

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -6,8 +6,8 @@ import linkedinIcon from '../../public/socialIcons/linkedin.png'
 export default function Social() {
   return (
     <ul className='h-8 flex flex-row mx-auto gap-2 lg:gap-6'>
-        <li><a href="https://www.linkedin.com/in/danielle-lindblom/" title='LinkedIn'><img src={linkedinIcon.src} className='w-8 hover:shadow-[0_0_5px_5px_#ffbd59] rounded-full transition duration-300 ease-in-out' alt=''/></a></li>
-        <li><a href="https://github.com/Danielle254" title='GitHub'><img src={githubIcon.src} className='w-8 hover:shadow-[0_0_5px_5px_#ffbd59] rounded-full transition duration-300 ease-in-out' alt=''/></a></li>
+        <li><a href="https://www.linkedin.com/in/danielle-lindblom/" title='LinkedIn' target='_blank'><img src={linkedinIcon.src} className='w-8 hover:shadow-[0_0_5px_5px_#ffbd59] rounded-full transition duration-300 ease-in-out' alt=''/></a></li>
+        <li><a href="https://github.com/Danielle254" title='GitHub' target='_blank'><img src={githubIcon.src} className='w-8 hover:shadow-[0_0_5px_5px_#ffbd59] rounded-full transition duration-300 ease-in-out' alt=''/></a></li>
         <li><a href="mailto:danielle.lindblom@gmail.com" title='Email'><img src={emailIcon.src} className='w-8 hover:shadow-[0_0_5px_5px_#ffbd59] rounded-full transition duration-300 ease-in-out' alt=''/></a></li>
     </ul>
 
