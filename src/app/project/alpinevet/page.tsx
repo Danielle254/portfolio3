577023ea08ba7358b613c0a542f2f48ec6d7d991
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-26 11:59:31 -0700

    fix: update copy on alpine vet page

--- src/app/project/alpinevet/page.tsx
+++ src/app/project/alpinevet/page.tsx
@@ -14,16 +14,14 @@ const alpineVetProject = {
   I wanted to create a more user-friendly, easy to navigate homepage for the veterinarian's customers.
   
   July 2024`,
-  heading: "Key Contributions",
-  features: ["Removed distracting, obsolete information from the site", "Gave users the ability to clear popup notifications", "Resolved a screen flickering issue by creating a new login authentication workflow and utilizing a loading spinner", "Identified and fixed a key accessibility issue with the menu controls"],
+  heading: "Before/After",
+  features: ["No semantic HTML / Updated to semantic HTML for better accessibility", "Inconsistent font size and color / Updated color palette based on logo", "Old, outdated page layout / New sectioned layout and better readability", "Contact information is non-interactive / Clickable links for phone, email, and address making it easier for visitors to use the information", "Very crowded navigation menu / Reorganized and reduced the navigation menu with better categories"],
   skills: ["Semantic HTML", "Pure CSS - FlexBox, Grid, Media Queries", "Web Design and User Experience", "Web Accessibility (NOTE: the checkbox hack, described below, is NOT fully web accessible)", "Responsive Design" ],
   learnings: `I used the "checkbox hack" for the mobile menu. I could have used JavaScript, but this was a fun feature to learn. The checkbox hack utilizes the "checked" vs "not checked" property of a checkbox instead of "onClick" in JavaScript. I also created the down/up arrows for the mobile submenu manually.
   
   I created a 2-layer menu. It was interesting to learn different methods of creating a submenu for both dektop and mobile views. This is a very common feature in older or complex business websites.`,
   future: `Creating a Site Menu in the footer. The footer is already quite full. I might make it it's own section and add a site menu to the actual footer on the desktop version.
   
-  I'd also like to add a "share" button in addition to printing.
-
   It would be fun to do an entire website overhaul and build the other pages.
 
   Add JavaScript for more simplified functionality of the navbar, and add a "return to top" icon for mobile.`,
