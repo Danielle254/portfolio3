19dc69f66c509bed41fb3d1a1defcfa3ddb9a173
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-26 09:35:51 -0700

    a11y: add video transcript

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -9,7 +9,8 @@ import CustomLink from '@/components/CustomLink';
 const caveat = Caveat({ subsets: ['latin'], weight: '400', variable: '--font-caveat' });
 
 export default function About() {
-  const [iframeLoading, setIframeLoading] = useState(true);
+  const [iframeLoading, setIframeLoading] = useState<boolean>(true);
+  const [showTranscript, setShowTranscript] = useState<boolean>(false);
 
   function hideLoadingSpinner () {
     setIframeLoading(false);
@@ -38,6 +39,15 @@ export default function About() {
                         /></div> : null}
         <iframe className='absolute w-full h-full left-0 top-0 border border-main-pink rounded' src="https://www.loom.com/embed/7780d925a77e48848bc75caff6d317c3?sid=fc473fda-2fc0-4b7e-b0ce-ba8106adae53?hideEmbedTopBar=true&hide_title=true&hide_owner=true" allowFullScreen title='Danielle Lindblom video introduction' onLoad={hideLoadingSpinner}></iframe>
       </div>
+      <button onClick={() => setShowTranscript(!showTranscript)} className='text-sm hover:underline ml-0'>Video Transcript</button>
+      {showTranscript &&
+      <p className='text-sm text-left'>0:01 Hey there, I'm Danielle Lindblom, a front-end developer from Missoula Montana. I specialize in building accessible websites using React and JavaScript, like my side project that helps service dog handlers navigate their local communities.
+      0:16 I'm currently diving deeper into TypeScript, NextJS, and Unit Testing in my apprenticeship program, where I'm working with my mentor to build and maintain a football survivor pool SaaS product.
+      0:29 Fun fact, I'm actually coming into software engineering after a successful career in manufacturing engineering and continuous improvement. I also spent several years running my own small businesses of professional dog training and life coaching.
+      0:44 Front-end development has been a great fit for me because of my focus on problem solving and people. And I'm particularly interested in building accessibility into all products from the start.
+      0:57 Please take a look at my body of work, which can be found on my linked portfolio, and use the attached contact information to get in touch about opportunities at your organization.
+      1:08 Thanks for taking the time to watch this video, and I look forward to chatting with you soon.</p>
+      }
       <p className='text-md md:text-lg mt-8'>I love working with <span className='text-accent-orange font-bold'>React,</span> <span className='text-accent-orange font-bold'>TypeScript,</span> and <span className='text-accent-orange font-bold'>Next.js.</span></p>
       <p className='text-md md:text-lg'>My background includes behavior science, project management, and instruction.</p>
       <div className='flex flex-row gap-8 justify-center mt-8'>
