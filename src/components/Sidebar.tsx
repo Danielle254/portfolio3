b00c84c5652a639314c15758308a578dbddfb237
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-26 10:09:03 -0700

    feat: link to homepage when clicking the profile picture in nav header

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -40,12 +40,14 @@ export default function Sidebar() {
           isOpen={drawerOpen}
           toggleDrawerOpen={toggleDrawerOpen}
         />
-        <Image
-          src={profile}
-          alt='Danielle Lindblom profile picture'
-          style={{borderRadius: '50%', border: '1px solid black'}}
-          height={50}
-        />
+        <Link href={'/'}>
+          <Image
+            src={profile}
+            alt='Danielle Lindblom profile picture'
+            style={{borderRadius: '50%', border: '1px solid black'}}
+            height={50}
+          />
+        </Link>
         <button onClick={toggleDrawerOpen}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
