e9f526514fb89cc70047add1f5a39eb6dcd415ff
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-27 12:22:24 -0700

    fix: text overflowing when transcript is visible

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -17,16 +17,25 @@ export default function About() {
   }
 
   return (
-    <motion.section
-    initial={{ opacity: 0}}
-    whileInView={{ opacity: 1}}
-    transition={{ duration: 0.7 }}
-    className={`h-full md:min-h-screen max-w-xl lg:max-w-2xl mx-auto py-8 px-4 ${showTranscript ? 'mb-64' : 'mb-32'} sm:mb-40 lg:mb-0 text-left`}
+    <section
+    className={`h-full md:min-h-screen max-w-xl lg:max-w-2xl mx-auto py-8 px-4 ${showTranscript ? 'mb-[700px] sm:mb-[500px]' : 'mb-40 sm:mb-32'} text-left`}
     id='about-section'
     >
-      <h2 className={`text-5xl ${caveat.variable} font-special my-4 md:mb-2 text-center`}>Hi, I'm Danielle</h2>
-      <p className='text-xl mb-8 text-center'>a <span className='font-bold'>Frontend Developer</span> passionate about accessibility.</p>
-      <div className='relative mx-auto h-0 pb-[56%]'>
+      <motion.h2 
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 0.7 }}
+      className={`text-5xl ${caveat.variable} font-special my-4 md:mb-2 text-center text-accent-orange`}>Hi, I'm Danielle</motion.h2>
+      <motion.p 
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 0.7 }}
+      className='text-xl mb-8 text-center'>a <span className='font-bold'>Frontend Developer</span> passionate about accessibility.</motion.p>
+      <motion.div 
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 0.7 }}
+      className='relative mx-auto h-0 pb-[56%]'>
         {iframeLoading ? 
                     <div className='flex flex-col items-center'>
                       <RotatingLines
@@ -38,18 +47,34 @@ export default function About() {
                         ariaLabel="rotating-lines-loading"
                         /></div> : null}
         <iframe className='absolute w-full h-full left-0 top-0 border border-main-pink rounded' src="https://www.loom.com/embed/7780d925a77e48848bc75caff6d317c3?sid=fc473fda-2fc0-4b7e-b0ce-ba8106adae53?hideEmbedTopBar=true&hide_title=true&hide_owner=true" allowFullScreen title='Danielle Lindblom video introduction' onLoad={hideLoadingSpinner}></iframe>
-      </div>
-      <button onClick={() => setShowTranscript(!showTranscript)} className='text-sm hover:underline'>Video Transcript</button>
+      </motion.div>
+      <motion.button 
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 0.7 }}
+      onClick={() => setShowTranscript(!showTranscript)} className='text-sm hover:underline cursor-pointer'>Video Transcript</motion.button>
       {showTranscript &&
-      <p className='text-sm text-left'>0:01 Hey there, I'm Danielle Lindblom, a front-end developer from Missoula Montana. I specialize in building accessible websites using React and JavaScript, like my side project that helps service dog handlers navigate their local communities.
+      <motion.p 
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 0.7 }}
+      className='text-sm text-left'>0:01 Hey there, I'm Danielle Lindblom, a front-end developer from Missoula Montana. I specialize in building accessible websites using React and JavaScript, like my side project that helps service dog handlers navigate their local communities.
       0:16 I'm currently diving deeper into TypeScript, NextJS, and Unit Testing in my apprenticeship program, where I'm working with my mentor to build and maintain a football survivor pool SaaS product.
       0:29 Fun fact, I'm actually coming into software engineering after a successful career in manufacturing engineering and continuous improvement. I also spent several years running my own small businesses of professional dog training and life coaching.
       0:44 Front-end development has been a great fit for me because of my focus on problem solving and people. And I'm particularly interested in building accessibility into all products from the start.
       0:57 Please take a look at my body of work, which can be found on my linked portfolio, and use the attached contact information to get in touch about opportunities at your organization.
-      1:08 Thanks for taking the time to watch this video, and I look forward to chatting with you soon.</p>
+      1:08 Thanks for taking the time to watch this video, and I look forward to chatting with you soon.</motion.p>
       }
-      <p className='text-md md:text-lg mt-8'>I love working with <span className='font-bold'>React,</span> <span className='font-bold'>TypeScript,</span> and <span className='font-bold'>Next.js</span>, and I have a diverse background in behavior science, project management, and technical instruction.</p>
-      <div className='flex flex-row gap-8 justify-start md:justify-center mt-10'>
+      <motion.p 
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 0.7 }}
+      className='text-md md:text-lg mt-8'>I love working with <span className='font-bold'>React,</span> <span className='font-bold'>TypeScript,</span> and <span className='font-bold'>Next.js</span>, and I have a diverse background in behavior science, project management, and technical instruction.</motion.p>
+      <motion.div 
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 0.7 }}
+      className='flex flex-row gap-8 justify-center mt-10'>
         <CustomLink
           text='View Projects'
           style='primary'
@@ -62,7 +87,7 @@ export default function About() {
           destination='/#connect-section'
           newWindow={false}
         />
-      </div>
-    </motion.section>
+      </motion.div>
+    </section>
   )
 }
