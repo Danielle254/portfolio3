585e049a8e099d52b65b039b4d7ca651850d1635
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-27 12:31:41 -0700

    fix: both mobile drawer control buttons in same location on screen

--- src/components/MobileDrawer.tsx
+++ src/components/MobileDrawer.tsx
@@ -13,8 +13,8 @@ type DrawerProps = {
 export default function MobileDrawer({isOpen, toggleDrawerOpen}: DrawerProps) {
     
   return (
-    <div className={`bg-gray-100 text-main-purple fixed top-0 right-0 flex flex-col justify-between py-8 items-center z-10 h-full w-full transition-transform duration-300 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-        <button className='self-end mr-4 border border-transparent hover:border-black' onClick={toggleDrawerOpen}>
+    <div className={`bg-gray-100 text-main-purple fixed top-0 right-0 flex flex-col justify-between py-6 items-center z-10 h-full w-full transition-transform duration-300 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+        <button className='self-end mr-6 border border-transparent hover:border-black' onClick={toggleDrawerOpen}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6" aria-hidden='true'>
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
