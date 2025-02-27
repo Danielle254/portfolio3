de665a6f43cdac6747473365f28f3dbcab03a33a
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-02-27 07:40:43 -0700

    fix: update URL for portfolio2 site

--- src/app/project/portfolio2/page.tsx
+++ src/app/project/portfolio2/page.tsx
@@ -26,7 +26,7 @@ const portfolio2Project = {
   links: [
     {
       title: "View Website",
-      link: 'https://staging.gridironsurvivor.com/',
+      link: 'https://danielle254.github.io/portfolio-site2/',
       style: 'primary'
     },
     {
